2c8fe2ad1ed75bcd3f386fecbe8bbb4a7c3b837f
Marcos Kruger <marcoskruger1002@gmail.com>
2023-07-05 16:42:37 -0300

    No trabalho

--- src/PrimeiraClasse.java
+++ src/PrimeiraClasse.java
@@ -1,3 +1,5 @@
+
+
 public class PrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Bem vindo ao MarcoslicoFlix");
@@ -8,7 +10,27 @@ public class PrimeiraClasse {
         System.out.println("Incluido no plano?");
 
         boolean incluidoNoPlano = true;
-        float notaDoFilme = 9.6;
+        double notaDoFilme = 9.6;
+
+        double media = (9.8 + 8.5 + 8) / 3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = """
+                Filme Viva, a vida é uma festa!
+                Filme incrível!
+                Ano de Lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
+
+
+        double temperaturaCelcius = 20;
+        double temperaturaFar = (temperaturaCelcius * 1.8) + 32;
+        System.out.println(temperaturaFar);
+
 
 
     }
