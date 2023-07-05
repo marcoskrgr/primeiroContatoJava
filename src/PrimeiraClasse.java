78a75c2abcae4dca2bc3ecce5b96565716538d32
Marcos Kruger <marcoskruger1002@gmail.com>
2023-07-05 11:00:02 -0300

    Att 1

---
+++ src/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao MarcoslicoFlix");
+        System.out.println("Filme: Viva, a vida é uma festa!");
+
+        int anoDeLancamento = 2017;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+
+
+    }
+}
