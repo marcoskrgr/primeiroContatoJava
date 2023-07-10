14da44f8d8daa2426296b38318b64d04657c0854
Marcos Kruger <marcoskruger1002@gmail.com>
2023-07-10 17:20:28 -0300

    No trabalho

--- src/Loop.java
+++ src/Loop.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Loop {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
+        double media = 0;
+        double nota = 0;
 
-        for (int i = 0; i < 2; i++) {
-
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Digite sua nota para o filme: ");
+            nota = leitura.nextDouble();
+            media += nota;
         }
 
-        System.out.println("De 0 a 10, qual sua nota para o filme?");
-        double avaliacao = leitura.nextDouble();
+        System.out.println("A média do seu filme é " + media/3);
+
     }
 }
